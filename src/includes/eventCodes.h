c38390bb2f49a7e0901f08ea16c23febb4b6b410
Luna <luna.kissameow@proton.me>
2024-09-17 11:09:45 +0300

    nyaaaa

--- src/includes/eventCodes.h
+++ src/includes/eventCodes.h
@@ -5,11 +5,11 @@ enum eventCodes{
   Heartbeat = 1,
   Identify = 2,
   PresenceUpdate = 3,
-  voiceStateUpdate = 4,
+  VoiceStateUpdate = 4,
   Resume = 6,
   Reconnect = 7,
   RequestGuildMember = 8,
-  invalidSession = 9,
+  InvalidSession = 9,
   Hello = 10,
   HeartbeatACK = 11
 };
