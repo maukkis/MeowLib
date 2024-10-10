d264b5d0914851126205fc93d31b762a183ace38
luna <luna.kissameow@proton.me>
2024-10-10 08:55:32 +0300

    removed accidental +1 to rlen

--- src/recv.cpp
+++ src/recv.cpp
@@ -1,3 +1,23 @@
+/* nyaBot simple discord bot written in C++ using libcurl
+    Copyright (C) 2024  Luna
+This file is part of nyaBot.
+
+nyaBot is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+nyaBot is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with nyaBot; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+
+
 #include <cstring>
 #include <fstream>
 #include <iostream>
@@ -62,7 +82,7 @@ void nyaBot::listen(){
         else {
           std::cout << "[*] got data: " << rlen1 << " bytes\n";
           buffer[rlen] = static_cast<char>(NULL);
-          rlen += rlen1 + 1;
+          rlen += rlen1;
           strncat(buffer, buffer1, rlen1);
           goto parseAgain;
         }
