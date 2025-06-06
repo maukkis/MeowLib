4143df0055bb09809d907c9e98bd3fc2750ca714
Luna <luna.kissameow@proton.me>
2025-06-07 01:19:45 +0300

    added clearing stdout buf

--- src/log.cpp
+++ src/log.cpp
@@ -1,11 +1,14 @@
 #include "../include/log.h"
 #include <chrono>
+#include <cstdio>
 #include <print>
 
 namespace Log {
   void Log(const std::string_view a){
-    if(enabled)
+    if(enabled){
        std::println(Log::fmtrstr, std::chrono::time_point_cast<std::chrono::seconds>(std::chrono::system_clock::now()), a);
+       std::fflush(stdout);     
+    }
   }
   bool enabled = false;
 }
