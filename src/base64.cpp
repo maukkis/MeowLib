e03cf4681a1f122124af5ea7a083ba8a0de6d10a
Luna <luna.kissameow@proton.me>
2025-09-17 14:42:09 +0300

    improve base64

--- src/base64.cpp
+++ src/base64.cpp
@@ -20,12 +20,12 @@ const std::unordered_map<uint8_t, char> base64_map = {
 };
 
 std::string encodeB64(const std::string_view a){
-  std::string_view str = a;
+  std::basic_string_view<uint8_t> str(reinterpret_cast<uint8_t const*>(a.data()), reinterpret_cast<uint8_t const*>(a.data() + a.size()));
   std::string encoded;
   encoded.reserve(a.length());
   while(str.length() > 0){
     if(str.length() >= 3){
-      const uint32_t val = (static_cast<uint8_t>(str[0]) << 16) | (static_cast<uint8_t>(str[1]) << 8) | static_cast<uint8_t>(str[2]);
+      const uint32_t val = (str.at(0) << 16) | (str.at(1) << 8) | str.at(2);
       for(ssize_t i = 3; i >= 0; --i){
         const uint8_t chr = (val & (0b00111111 << i * 6)) >> i * 6;
         encoded += base64_map.at(chr);
@@ -33,7 +33,7 @@ std::string encodeB64(const std::string_view a){
       str = str.substr(3);
     }
     else if(str.length() == 2){
-      const uint32_t val = (static_cast<uint8_t>(str[0]) << 16) | static_cast<uint8_t>(str[1]) << 8;
+      const uint32_t val = (str.at(0) << 16) | str.at(1) << 8;
       for(ssize_t i = 3; i > 0; --i){
         const uint8_t chr = (val & (0b00111111 << i * 6)) >> i * 6;
         encoded += base64_map.at(chr);
@@ -42,7 +42,7 @@ std::string encodeB64(const std::string_view a){
       break;
     }
     else if(str.length() == 1){
-      const uint8_t val = static_cast<uint8_t>(str[0]);
+      const uint8_t val = str.at(0);
       const uint8_t chr = (val & 0b11111100) >> 2;
       encoded += base64_map.at(chr);
       const uint8_t chr2 = (val & 0b00000011) << 4;
