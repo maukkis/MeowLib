ac62f7f0464d2f861228d130175fe1fce46ee47c
Luna <luna@girlsmell.xyz>
2025-09-25 18:41:55 +0300

    change logging colors

--- include/log.h
+++ include/log.h
@@ -8,8 +8,8 @@ namespace Log {
   void info(const std::string_view a);
   void error(const std::string_view a);
   constexpr std::string_view dbgfmtrstr = "\e[38;5;212mDBG   {:%T} | \e[0;37m {}";
-  constexpr std::string_view warnfmtrstr = "\e[38;5;212mWarn  {:%T} | \e[0;37m {}";
-  constexpr std::string_view errfmtrstr = "\e[38;5;212mError {:%T} | \e[0;37m {}";
+  constexpr std::string_view warnfmtrstr = "\e[0;33mWarn  {:%T} | \e[0;37m {}";
+  constexpr std::string_view errfmtrstr = "\e[0;31mError {:%T} | \e[0;37m {}";
   constexpr std::string_view infofmtrstr = "\e[38;5;212mInfo  {:%T} | \e[0;37m {}";
   extern bool enabled;
 }
