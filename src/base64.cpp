3753c4355fda4ad839b0600e0069cb4dadee8560
Luna <luna@girlsmell.xyz>
2026-04-27 14:15:20 +0300

    fix base64 encoder throwing warnings on macos

--- src/base64.cpp
+++ src/base64.cpp
@@ -19,12 +19,12 @@ constexpr std::array<uint8_t, 64> base64_map = {
 
 
 std::string encodeB64(const std::string_view a){
-  std::basic_string_view<uint8_t> str(reinterpret_cast<uint8_t const*>(a.data()), a.size());
+  std::string_view str(a);
   std::string encoded;
   encoded.reserve(a.length());
   while(str.length() > 0){
     if(str.length() >= 3){
-      const uint32_t val = (str.at(0) << 16) | (str.at(1) << 8) | str.at(2);
+      const uint32_t val = (static_cast<uint8_t>(str.at(0)) << 16) | (static_cast<uint8_t>(str.at(1)) << 8) | static_cast<uint8_t>(str.at(2));
       for(int i = 3; i >= 0; --i){
         const uint8_t chr = (val & (0b00111111 << i * 6)) >> i * 6;
         encoded += base64_map.at(chr);
@@ -32,7 +32,7 @@ std::string encodeB64(const std::string_view a){
       str = str.substr(3);
     }
     else if(str.length() == 2){
-      const uint32_t val = (str.at(0) << 16) | str.at(1) << 8;
+      const uint32_t val = (static_cast<uint8_t>(str.at(0)) << 16) | static_cast<uint8_t>(str.at(1)) << 8;
       for(int i = 3; i > 0; --i){
         const uint8_t chr = (val & (0b00111111 << i * 6)) >> i * 6;
         encoded += base64_map.at(chr);
@@ -41,7 +41,7 @@ std::string encodeB64(const std::string_view a){
       break;
     }
     else if(str.length() == 1){
-      const uint8_t val = str.at(0);
+      const uint8_t val = static_cast<uint8_t>(str.at(0));
       const uint8_t chr = (val & 0b11111100) >> 2;
       encoded += base64_map.at(chr);
       const uint8_t chr2 = (val & 0b00000011) << 4;
